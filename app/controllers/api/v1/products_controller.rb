3030a2373d24c39e29c47cf1341db50a52d505db
Essilfie <bquansah007@gmail.com>
2023-11-12 18:28:50 +0000

    Update products_controller.rb

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,18 +1,20 @@
 class API::V1::ProductsController < APIController
 
     def index
-      @products = Product.filter(params.slice(:published, :product_name, :product_description, :product_price_min, :product_price_max))
-      
-      if @products.present?
-        product_response = ProductSerializer.new(@products).serializable_hash[:data].map{|p| p[:attributes]}
-        render json: {data: product_response}, status: :ok
-      else
-        render json: { message: "No products found for the given criteria" }, status: :not_found
-      end
+      @products = Product.filter(filter_params)
+        
+      product_response = ProductSerializer.new(@products).serializable_hash[:data].map{|p| p[:attributes]}
+      render json: { data: product_response }, status: :ok
     end
 
     def show
         @product = Product.find(params[:id])
         render json: ProductSerializer.new(@product).serializable_hash[:data][:attributes], status: :ok
+    end
+
+    private
+
+    def filter_params
+      params.permit(:name, :description, :price_min, :price_max)
     end
 end
