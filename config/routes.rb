f1fd9a62675f74117f4eca4b10475f668c868479
Essilfie <bquansah007@gmail.com>
2023-11-12 18:38:11 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, path: '/' do
-      resources :users
+      resource :users
       resources :products, only: %i[index show]
 
 
